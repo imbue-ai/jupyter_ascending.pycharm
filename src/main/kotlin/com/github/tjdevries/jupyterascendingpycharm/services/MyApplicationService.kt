d5d0184a16544e8775093e726b982f4939daa198
GitHub Action <action@github.com>
2020-08-07 16:42:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tjdevries/jupyterascendingpycharm/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tjdevries.jupyterascendingpycharm.services
+
+import com.github.tjdevries.jupyterascendingpycharm.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
